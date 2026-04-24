5e765e72dc966185ad844e759fdec4d6ed4a5979
chenxu10 <shenxu2011@gmail.com>
2026-04-24 05:44:01 -0400

    strategy maker

--- src/plvisualizer/plotdatabuilder.py
+++ src/plvisualizer/plotdatabuilder.py
@@ -4,4 +4,11 @@ class PLPlotDataBuilder:
         self.position = position
 
     def calculate_actual_pl_range(self):
-        return [5 - 100 * 100, 5 - 50 * 100, 5, 5]
+        
+        pnl_range = []
+        
+        for s in self.stock_price_range:
+            pnl_at_s = sum(leg.pnl_at(s) for leg in self.position.legs)
+            pnl_range.append(pnl_at_s)  
+            
+        return pnl_range
